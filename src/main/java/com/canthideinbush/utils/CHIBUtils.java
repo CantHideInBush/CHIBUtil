053af60df8f11c66b0e4646e76698922ac48fa09
KarwszPL <karwszpl@gmail.com>
2022-11-26 19:26:32 +0100

    A lot.

--- src/main/java/com/canthideinbush/utils/CHIBUtils.java
+++ src/main/java/com/canthideinbush/utils/CHIBUtils.java
@@ -1,10 +1,13 @@
 package com.canthideinbush.utils;
 
+import com.canthideinbush.utils.gui.shop.BasicShopGUI;
 import com.canthideinbush.utils.storing.items.AxolotlBucketValues;
 import com.canthideinbush.utils.storing.items.HRItem;
 import com.canthideinbush.utils.storing.YAMLConfig;
 import com.canthideinbush.utils.tempblock.TempBlockManager;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.jetbrains.annotations.NotNull;
 
 public class CHIBUtils extends CHIBPlugin {
@@ -12,12 +15,13 @@ public class CHIBUtils extends CHIBPlugin {
     static {
         ConfigurationSerialization.registerClass(HRItem.class);
         ConfigurationSerialization.registerClass(AxolotlBucketValues.class);
+        ConfigurationSerialization.registerClass(BasicShopGUI.class);
     }
 
     private YAMLConfig config;
     private YAMLConfig messageConfig;
 
-    private UtilsProvider utilsProvider;
+    public static Economy vaultEconomy;
 
 
 
@@ -33,12 +37,17 @@ public class CHIBUtils extends CHIBPlugin {
     public void onEnable() {
         instance = this;
 
+        if (!setupEconomy()) {
+            getLogger().warning("Economy plugin not found! Basic shop gui's are disabled.");
+        }
+
         config = new YAMLConfig(this, "config", true);
         messageConfig = new YAMLConfig(this, "default_messages", true);
 
         CHIBInit();
 
         this.tempBlockManager = new TempBlockManager();
+
 
     }
 
@@ -66,6 +75,18 @@ public class CHIBUtils extends CHIBPlugin {
     @Override
     public UtilsProvider getUtilsProvider() {
         return utilsProvider;
+    }
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        vaultEconomy = rsp.getProvider();
+        return true;
     }
 
 }
