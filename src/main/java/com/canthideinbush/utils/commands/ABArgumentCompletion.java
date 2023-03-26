df5f70e69adb367dd3d998b0fe5190779439ba52
karwsz <karwszpl@gmail.com>
2023-03-26 23:14:06 +0200

    ABCompletion feature

--- src/main/java/com/canthideinbush/utils/commands/ABArgumentCompletion.java
+++ src/main/java/com/canthideinbush/utils/commands/ABArgumentCompletion.java
@@ -1,8 +1,13 @@
 package com.canthideinbush.utils.commands;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.function.Function;
 
 public interface ABArgumentCompletion {
 
@@ -10,13 +15,39 @@ public interface ABArgumentCompletion {
 
 
 
+    default List<HashMap<String, Method>> prepareCompletion() {
+        List<HashMap<String, Method>> completion = new ArrayList<>();
 
-    default List<List<String>> prepareCompletion() {
-        List<List<String>> completion = new ArrayList<>();
+        for (Method method : this.getClass().getDeclaredMethods()) {
+            if (method.isAnnotationPresent(ABCompleter.class)) {
+                ABCompleter completer = method.getAnnotation(ABCompleter.class);
+                HashMap<String, Method> map;
+                if (completion.size() < completer.i()) {
+                    map = new HashMap<>();
+                    completion.set(completer.i(), map);
+                } else map = completion.get(completer.i());
+                map.put(completer.argument(), method);
+            }
 
-        for (Annotation annotation : this.getClass().getDeclaredAnnotations())
-
+        }
         return completion;
+    }
+
+    List<HashMap<String, Method>> getCompletion();
+
+
+    int getArgIndex();
+
+    default List<String> ABComplete(String[] args) {
+        int index = args.length - getArgIndex() + 1;
+        if (index < 0 || getCompletion().size() <= index) return Collections.emptyList();
+        HashMap<String, Method> map = getCompletion().get(index);
+        if (!map.containsKey(args[args.length - 1])) return  Collections.emptyList();
+        try {
+            return (List<String>) map.get(args[args.length - 1]).invoke(this);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
---
+++ src/main/java/com/canthideinbush/utils/commands/ABCompleter.java
@@ -0,0 +1,15 @@
+package com.canthideinbush.utils.commands;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ABCompleter {
+
+    int index();
+
+    String arg();
+
+
+
+}
